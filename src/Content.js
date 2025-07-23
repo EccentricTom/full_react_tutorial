ea9a51412a9e80e50214d820d79cc0e52b694d19
Tom Oliver <tom@inmail.me>
2025-07-23 12:18:56 +0200

    Learnt about button clicks at basic level (very similar to how NiceGui handles events, shows js throughline of thought)

--- src/Content.js
+++ src/Content.js
@@ -2,16 +2,31 @@ import React from "react";
 
 const Content = () => {
   const handleNameChange = () => {
-    const names = ["blerg", "blorg", "blarg", "blirg"];
+    const names = ["Blerg", "Blorg", "Blarg", "Blirg"];
     const int = Math.floor(Math.random() * 4);
     return names[int];
   };
 
+  const handleClick = () => {
+    console.log("Clicked!")
+  }
+  
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`)
+  }
+  
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText)
+  }
+
   return (
     <main>
-      <p>
+      <p on onDoubleClick={handleClick}>
         Hello {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Click It</button>
+        <button onClick={() => handleClick2("Tom")}>Click Name</button>
+        <button onClick={(e) => handleClick3(e)}>Click Name</button>
     </main>
   );
 };
