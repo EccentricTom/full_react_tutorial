89bf7db7733e97768f65f380648bf054655972e1
Tom Oliver <tom@inmail.me>
2025-07-23 11:45:03 +0200

    in-line formatting with new constant tried out

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,17 @@
 const Header = () => {
+  const headerStyle = {
+    backgroundColor: "mediumblue",
+    color: "white"
+  };
+
+
   return (
-    <header style = {{
-      backgroundColor: "mediumblue",
-      color: "white"
-    }}>
+    <header
+      style={headerStyle}
+    >
       <h1>Groceries List</h1>
     </header>
   );
-}
+};
 
 export default Header;
