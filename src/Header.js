9b8c0d8fdc447fd1516b06f6f1e3c0a00dfabff9
Tom Oliver <tom@inmail.me>
2025-07-23 11:33:57 +0200

    Experimented with in-line styling

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,9 @@
-import React from 'react';
-
 const Header = () => {
   return (
-    <header>
+    <header style = {{
+      backgroundColor: "mediumblue",
+      color: "white"
+    }}>
       <h1>Groceries List</h1>
     </header>
   );
