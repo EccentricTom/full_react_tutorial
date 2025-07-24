5f04d9686746271a8c3ccf958b6283d0d46cacad
Tom Oliver <tom@inmail.me>
2025-07-24 16:22:11 +0200

    chapter 9: learnt about forms and again the use of state to have a single source of truth. What is interesting is that it defaults to local instance only as opposed to how NiceGui handles it

--- src/App.js
+++ src/App.js
@@ -1,46 +1,59 @@
 import Header from "./Header";
+import SearchItem from "./SearchItem";
+import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
-import { useState } from "react";
+import { use, useState } from "react";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "Carton of Milk",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Box of twelve eggs",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Bag of sultanas",
-    },
-  ]);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shopping list"))
+  );
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("shopping list", JSON.stringify(newItems));
+  };
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const newGroceryItem = { id, checked: false, item };
+    const listItems = [...items, newGroceryItem];
+    setAndSaveItems(listItems);
+  };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setItems(listItems);
-    localStorage.setItem("shopping list", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shopping list", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem("");
   };
 
   return (
     <div className="App">
       <Header title="Grocery List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
       <Content
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
