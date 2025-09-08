a6fb79c4bcee563e7ecbb2c3cdc05e47a7824cb9
Tom Oliver <tom@inmail.me>
2025-09-08 17:07:03 +0200

    introduced to useEffect, which can be set to conditionally run at say startup and is a more efficient means of saving to local storage, including if the value does not exist at that point in time

--- src/App.js
+++ src/App.js
@@ -3,63 +3,65 @@ import SearchItem from "./SearchItem";
 import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
-import { use, useState } from "react";
+import { useState, useEffect, use } from "react";
 
 function App() {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shopping list"))
-  );
-  const [newItem, setNewItem] = useState("");
-  const [search, setSearch] = useState("");
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppinglist")) || [])
+	const [newItem, setNewItem] = useState("");
+	const [search, setSearch] = useState("");
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem("shopping list", JSON.stringify(newItems));
-  };
+  useEffect(() => {
+    localStorage.setItem("shoppinglist", JSON.stringify(items))
+  }, [items])
 
-  const addItem = (item) => {
-    const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const newGroceryItem = { id, checked: false, item };
-    const listItems = [...items, newGroceryItem];
-    setAndSaveItems(listItems);
-  };
+	const addItem = (item) => {
+		const id = items.length ? items[items.length - 1].id + 1 : 1;
+		const newGroceryItem = { id, checked: false, item };
+		const listItems = [...items, newGroceryItem];
+		setItems(listItems);
+	};
 
-  const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
-    setAndSaveItems(listItems);
-  };
+	const handleCheck = (id) => {
+		const listItems = items.map((item) =>
+			item.id === id ? { ...item, checked: !item.checked } : item
+		);
+		setItems(listItems);
+	};
 
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
-  };
+	const handleDelete = (id) => {
+		const listItems = items.filter((item) => item.id !== id);
+		setItems(listItems);
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!newItem) return;
-    addItem(newItem);
-    setNewItem("");
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!newItem) return;
+		addItem(newItem);
+		setNewItem("");
+	};
 
-  return (
-    <div className="App">
-      <Header title="Grocery List" />
-      <AddItem
-        newItem={newItem}
-        setNewItem={setNewItem}
-        handleSubmit={handleSubmit}
-      />
-      <SearchItem search={search} setSearch={setSearch} />
-      <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
-      <Footer length={items.length} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header title="Grocery List" />
+			<AddItem
+				newItem={newItem}
+				setNewItem={setNewItem}
+				handleSubmit={handleSubmit}
+			/>
+			<SearchItem
+				search={search}
+				setSearch={setSearch}
+			/>
+			<Content
+				items={items.filter((item) =>
+					item.item.toLowerCase().includes(search.toLowerCase())
+				)}
+				handleCheck={handleCheck}
+				handleDelete={handleDelete}
+			/>
+			<Footer length={items.length} />
+		</div>
+	);
 }
 
 export default App;
