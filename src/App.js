d5aabb992d80ca6087a0d3860970dd6837f97c86
Tom Oliver <tom@inmail.me>
2025-09-08 17:39:59 +0200

    Added using async to fetch from an api and error handling syntaxes (including how to best inform the user of the syntax issue)

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import SearchItem from "./SearchItem";
 import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppinglist")) || [])
+	const API_URL = "http://localhost:3500/items";
+
+	const [items, setItems] = useState([]);
 	const [newItem, setNewItem] = useState("");
 	const [search, setSearch] = useState("");
+	const [fetchError, setFetchError] = useState(null);
 
-  useEffect(() => {
-    localStorage.setItem("shoppinglist", JSON.stringify(items))
-  }, [items])
+	useEffect(() => {
+		const fetchItems = async () => {
+			try {
+				const response = await fetch(API_URL);
+				if (!response.ok) throw Error("Did not recieve expected data");
+				const listItems = await response.json();
+				console.log(listItems);
+				setItems(listItems);
+				setFetchError(null);
+			} catch (err) {
+				setFetchError(err.message);
+			}
+		};
+
+		(async () => await fetchItems())();
+	}, []);
 
 	const addItem = (item) => {
 		const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -52,13 +68,16 @@ function App() {
 				search={search}
 				setSearch={setSearch}
 			/>
-			<Content
-				items={items.filter((item) =>
-					item.item.toLowerCase().includes(search.toLowerCase())
-				)}
-				handleCheck={handleCheck}
-				handleDelete={handleDelete}
-			/>
+			<main>
+				{fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+				{!fetchError &&<Content
+					items={items.filter((item) =>
+						item.item.toLowerCase().includes(search.toLowerCase())
+					)}
+					handleCheck={handleCheck}
+					handleDelete={handleDelete}
+				/>}
+			</main>
 			<Footer length={items.length} />
 		</div>
 	);
