6fa57a776a82afd47ce1d6940a5c8bfa2e85a492
Tom Oliver <tom@inmail.me>
2025-09-08 16:26:10 +0200

    followed instructions on the colour change application challenge (did not attempt myself as it had been too long since last practice, but good refresher)

---
+++ colour_change_app/src/App.js
@@ -0,0 +1,27 @@
+import Square from "./Square";
+import Input from "./Input";
+import { useState } from "react";
+
+function App() {
+	const [colorValue, setColorValue] = useState("");
+	const [hexValue, setHexValue] = useState("");
+  const [isDarkText, setIsDarkText] = useState(true)
+	return (
+		<div className="App">
+			<Square
+				colorValue={colorValue}
+				hexValue={hexValue}
+        isDarkText={isDarkText}
+			></Square>
+			<Input
+				colorValue={colorValue}
+				setColorValue={setColorValue}
+        setHexValue = {setHexValue}
+        isDarkText={isDarkText}
+        setIsDarkText={setIsDarkText}
+			></Input>
+		</div>
+	);
+}
+
+export default App;
